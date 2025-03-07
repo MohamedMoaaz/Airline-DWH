034f33c8224d6ec6a7dd00a067db2f965d1217e1
Mohamed Ashraf <mohamed.ashraf211087@gmail.com>
2025-03-08 00:43:39 +0200

    Add the sql script to populate the crew-employee bridge

---
+++ physical schema/population/populate_crew_employee_bridge.sql
@@ -0,0 +1,6 @@
+INSERT INTO crew_employee_bridge (crew_employee_bridge_key, crew_key, employee_key) VALUES (1, 1, 1);
+INSERT INTO crew_employee_bridge (crew_employee_bridge_key, crew_key, employee_key) VALUES (2, 1, 2);
+INSERT INTO crew_employee_bridge (crew_employee_bridge_key, crew_key, employee_key) VALUES (3, 2, 3);
+INSERT INTO crew_employee_bridge (crew_employee_bridge_key, crew_key, employee_key) VALUES (4, 2, 4);
+INSERT INTO crew_employee_bridge (crew_employee_bridge_key, crew_key, employee_key) VALUES (5, 3, 5);
+INSERT INTO crew_employee_bridge (crew_employee_bridge_key, crew_key, employee_key) VALUES (6, 3, 6);
