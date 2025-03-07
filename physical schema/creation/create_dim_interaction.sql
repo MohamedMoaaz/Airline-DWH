a03f413bef57c5c8f592f4b05ef828b48b935a93
Mohamed Ashraf <mohamed.ashraf211087@gmail.com>
2025-03-08 00:38:02 +0200

    Add the sql script to create the dim_interaction table

---
+++ physical schema/creation/create_dim_interaction.sql
@@ -0,0 +1,7 @@
+CREATE TABLE dim_interaction (
+    interaction_key INT,
+    interaction_type VARCHAR2(255),
+    severity VARCHAR2(255),
+    engine_type VARCHAR2(255),
+    status VARCHAR2(255)
+);
