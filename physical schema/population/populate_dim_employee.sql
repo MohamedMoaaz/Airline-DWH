9f8cf0adbaf9e46dc35b674e810202ca41d3394d
Mohamed Ashraf <mohamed.ashraf211087@gmail.com>
2025-03-08 00:44:18 +0200

    Add the sql script to populate the dim_employee table

---
+++ physical schema/population/populate_dim_employee.sql
@@ -0,0 +1,12 @@
+INSERT INTO employee (employee_key, employee_id, employee_first_name, employee_last_name, employee_email, employee_phone_number, employee_job_title, employee_salary, employee_hiring_date)
+VALUES (1, 1001, 'John', 'Doe', 'john.doe@example.com', '1234567890', 'Engineer', 75000, TO_DATE('2023-06-15', 'YYYY-MM-DD'));
+INSERT INTO employee (employee_key, employee_id, employee_first_name, employee_last_name, employee_email, employee_phone_number, employee_job_title, employee_salary, employee_hiring_date)
+VALUES (2, 1002, 'Jane', 'Smith', 'jane.smith@example.com', '0987654321', 'Technician', 60000, TO_DATE('2022-03-10', 'YYYY-MM-DD'));
+INSERT INTO employee (employee_key, employee_id, employee_first_name, employee_last_name, employee_email, employee_phone_number, employee_job_title, employee_salary, employee_hiring_date)
+VALUES (3, 1003, 'Emily', 'Clark', 'emily.clark@example.com', '1122334455', 'Manager', 90000, TO_DATE('2021-09-25', 'YYYY-MM-DD'));
+INSERT INTO employee (employee_key, employee_id, employee_first_name, employee_last_name, employee_email, employee_phone_number, employee_job_title, employee_salary, employee_hiring_date)
+VALUES (4, 1004, 'Michael', 'Brown', 'michael.brown@example.com', '6677889900', 'Technician', 58000, TO_DATE('2020-05-20', 'YYYY-MM-DD'));
+INSERT INTO employee (employee_key, employee_id, employee_first_name, employee_last_name, employee_email, employee_phone_number, employee_job_title, employee_salary, employee_hiring_date)
+VALUES (5, 1005, 'Laura', 'Wilson', 'laura.wilson@example.com', '3344556677', 'Supervisor', 85000, TO_DATE('2019-11-11', 'YYYY-MM-DD'));
+INSERT INTO employee (employee_key, employee_id, employee_first_name, employee_last_name, employee_email, employee_phone_number, employee_job_title, employee_salary, employee_hiring_date)
+VALUES (6, 1006, 'Daniel', 'Martinez', 'daniel.martinez@example.com', '7788990011', 'Analyst', 72000, TO_DATE('2021-01-15', 'YYYY-MM-DD'));
