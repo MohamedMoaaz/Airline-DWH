4a3e9d3837cdaac74ff0b7249221603d667e5a79
Mohamed Ashraf <mohamed.ashraf211087@gmail.com>
2025-03-08 00:37:39 +0200

    Add the sql script to create the dim_employee table

---
+++ physical schema/creation/create_dim_employee.sql
@@ -0,0 +1,11 @@
+CREATE TABLE dim_employee (
+    employee_key INT,
+    employee_id INT,
+    employee_first_name VARCHAR2(255),
+    employee_last_name VARCHAR2(255),
+    employee_email VARCHAR2(255),
+    employee_phone_number VARCHAR2(20),
+    employee_job_title VARCHAR2(255),
+    employee_salary NUMBER(10,2),
+    employee_hiring_date DATE
+);
