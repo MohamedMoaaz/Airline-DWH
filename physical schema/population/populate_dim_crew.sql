82ca0c792e156799ff1349ae2f899731b5329760
Mohamed Ashraf <mohamed.ashraf211087@gmail.com>
2025-03-08 00:44:04 +0200

    Add the sql script to populate the dim_crew table

---
+++ physical schema/population/populate_dim_crew.sql
@@ -0,0 +1,6 @@
+INSERT INTO dim_crew (crew_key, crew_member_count, professional_level) VALUES (1, 5, 'Senior');
+INSERT INTO dim_crew (crew_key, crew_member_count, professional_level) VALUES (2, 3, 'Junior');
+INSERT INTO dim_crew (crew_key, crew_member_count, professional_level) VALUES (3, 4, 'Intermediate');
+INSERT INTO dim_crew (crew_key, crew_member_count, professional_level) VALUES (4, 6, 'Expert');
+INSERT INTO dim_crew (crew_key, crew_member_count, professional_level) VALUES (5, 2, 'Trainee');
+INSERT INTO dim_crew (crew_key, crew_member_count, professional_level) VALUES (6, 7, 'Master');
