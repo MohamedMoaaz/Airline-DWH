e2922f7b0f01bc35fef94a39871ec93022ef9edc
Randa Yasser <40809557+RandaYasser@users.noreply.github.com>
2025-03-10 23:34:17 +0200

    Update create_dim_passenger_profile_current.sql

--- physical schema/creation/create_dim_passenger_profile_current.sql
+++ physical schema/creation/create_dim_passenger_profile_current.sql
@@ -2,5 +2,6 @@ CREATE TABLE dim_passenger_profile (
     profile_key NUMBER,
     frequent_flyer_tier VARCHAR2(50),
     home_airport VARCHAR2(100),
-    lifetime_mileage_tier VARCHAR2(50)
+    lifetime_mileage_tier VARCHAR2(50),
+    updated_date DATE
 );
