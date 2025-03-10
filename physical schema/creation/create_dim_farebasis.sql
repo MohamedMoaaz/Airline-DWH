ce8e198255feca30aab19941a4c12716e243f6c8
Mennatullahatta <https://github.com/Mennatullahatta>
2025-03-11 00:56:54 +0200

    create dim_farebasis table

---
+++ physical schema/creation/create_dim_farebasis.sql
@@ -0,0 +1,14 @@
+CREATE TABLE dim_farebasis (
+    farebasis_key NUMBER,
+    farebasis_code VARCHAR2(50) ,
+    farebasis_class VARCHAR2(50),
+    farebasis_type VARCHAR2(50),
+    advance_purchase_required CHAR(1),
+    advance_purchase_days NUMBER,
+    farebasis_is_restricted CHAR(1),
+    farebasis_restrictions VARCHAR2(500),
+    farebasis_change_penalty NUMBER(10, 2),
+    farebasis_cancellation_penalty NUMBER(10, 2)
+);
+
+
