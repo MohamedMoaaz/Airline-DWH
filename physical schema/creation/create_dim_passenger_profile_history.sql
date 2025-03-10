e3bde45adeccd4a0d6184b31c852b21d970a2e16
Randa Yasser <40809557+RandaYasser@users.noreply.github.com>
2025-03-10 22:32:42 +0200

    Create create_dim_passenger_profile_history.sql

---
+++ physical schema/creation/create_dim_passenger_profile_history.sql
@@ -0,0 +1,9 @@
+CREATE TABLE dim_passenger_profile_history (
+    profile_history_key NUMBER
+    profile_key NUMBER,
+    frequent_flyer_tier VARCHAR2(50),
+    home_airport VARCHAR2(100),
+    lifetime_mileage_tier VARCHAR2(50),
+    start_date Date,
+    end_date Date
+);
