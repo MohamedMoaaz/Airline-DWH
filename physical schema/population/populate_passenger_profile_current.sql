630c1c019921da38b825ef5ad0de8527c3bf8a46
Randa Yasser <40809557+RandaYasser@users.noreply.github.com>
2025-03-10 23:14:09 +0200

    Create populate_passenger_profile_current.sql

---
+++ physical schema/population/populate_passenger_profile_current.sql
@@ -0,0 +1,39 @@
+-- Profile 1
+INSERT INTO dim_passenger_profile (profile_key, frequent_flyer_tier, home_airport, lifetime_mileage_tier)
+VALUES (1, 'Gold', 'JFK', 'Under 100,000 miles');
+
+-- Profile 2
+INSERT INTO dim_passenger_profile (profile_key, frequent_flyer_tier, home_airport, lifetime_mileage_tier)
+VALUES (2, 'Silver', 'LHR', 'Under 100,000 miles');
+
+-- Profile 3
+INSERT INTO dim_passenger_profile (profile_key, frequent_flyer_tier, home_airport, lifetime_mileage_tier)
+VALUES (3, 'Platinum', 'YYZ', '100,000-499,999 miles');
+
+-- Profile 4
+INSERT INTO dim_passenger_profile (profile_key, frequent_flyer_tier, home_airport, lifetime_mileage_tier)
+VALUES (4, 'Bronze', 'SYD', 'Under 100,000 miles');
+
+-- Profile 5
+INSERT INTO dim_passenger_profile (profile_key, frequent_flyer_tier, home_airport, lifetime_mileage_tier)
+VALUES (5, 'Gold', 'FRA', '100,000-499,999 miles');
+
+-- Profile 6
+INSERT INTO dim_passenger_profile (profile_key, frequent_flyer_tier, home_airport, lifetime_mileage_tier)
+VALUES (6, 'Silver', 'CDG', '100,000-499,999 miles');
+
+-- Profile 7
+INSERT INTO dim_passenger_profile (profile_key, frequent_flyer_tier, home_airport, lifetime_mileage_tier)
+VALUES (7, 'Platinum', 'NRT', '1,000,000-1,999,999 miles');
+
+-- Profile 8
+INSERT INTO dim_passenger_profile (profile_key, frequent_flyer_tier, home_airport, lifetime_mileage_tier)
+VALUES (8, 'Bronze', 'DXB', 'Under 100,000 miles');
+
+-- Profile 9
+INSERT INTO dim_passenger_profile (profile_key, frequent_flyer_tier, home_airport, lifetime_mileage_tier)
+VALUES (9, 'Gold', 'JFK', '100,000-499,999 miles');
+
+-- Profile 10
+INSERT INTO dim_passenger_profile (profile_key, frequent_flyer_tier, home_airport, lifetime_mileage_tier)
+VALUES (10, 'Silver', 'LHR', 'Under 100,000 miles');
