5f883588f5774ccefd2d86b269223bb6ed2fbe9b
Mennatullahatta <mennaatta94@gmail.com>
2025-03-11 16:08:18 +0200

    populate fact_points_snapshot

---
+++ physical schema/fact_points_snapshot/populate_fact_points_snapshot.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO fact_points_snapshot VALUES (1, 101, 20231031, 200);
+INSERT INTO fact_points_snapshot VALUES (2, 102, 20231031, 300); 
+INSERT INTO fact_points_snapshot VALUES (3, 103, 20231130, 1000); 
+COMMIT;
