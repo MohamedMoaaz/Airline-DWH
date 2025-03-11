22d0983552857351735bb1eb572ef62f9be0fc76
Mennatullahatta <mennaatta94@gmail.com>
2025-03-11 14:32:20 +0200

    create_fact_points

---
+++ physical schema/fact_points/create_fact_points.sql
@@ -0,0 +1,16 @@
+ CREATE TABLE fact_points (
+    points_key NUMBER,
+    passenger_key NUMBER,
+    passenger_profile_key NUMBER,
+    transaction_date_key NUMBER,
+    expiration_date_key NUMBER,
+    aircraft_key NUMBER,
+    crew_key  NUMBER,
+    crew_employee_bridge_key NUMBER,
+    service_key NUMBER,
+    promotion_key NUMBER,
+    points_earned NUMBER,
+    points_redeemed NUMBER,
+    points_expired NUMBER
+    
+);
