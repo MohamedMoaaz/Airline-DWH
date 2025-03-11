9127e8718da5af66acfe5661a894a5351c9f0598
Randa Yasser <40809557+RandaYasser@users.noreply.github.com>
2025-03-11 22:45:06 +0200

    Update create_dim_channel.sql
    removed inserts

--- physical schema/dim_channel/create_dim_channel.sql
+++ physical schema/dim_channel/create_dim_channel.sql
@@ -4,33 +4,3 @@ CREATE TABLE dim_channel (
     channel_type VARCHAR(50)
 );
 
-
-INSERT INTO dim_channel (channel_key, channel_name, channel_type)
-VALUES (1, 'Website', 'Online');
-
-INSERT INTO dim_channel (channel_key, channel_name, channel_type)
-VALUES (2, 'Mobile App', 'Online');
-
-INSERT INTO dim_channel (channel_key, channel_name, channel_type)
-VALUES (3, 'Travel Agency', 'Agent');
-
-INSERT INTO dim_channel (channel_key, channel_name, channel_type)
-VALUES (4, 'Call Center', 'Offline');
-
-INSERT INTO dim_channel (channel_key, channel_name, channel_type)
-VALUES (5, 'Airport Ticket Counter', 'Offline');
-
-INSERT INTO dim_channel (channel_key, channel_name, channel_type)
-VALUES (6, 'Social Media', 'Online');
-
-INSERT INTO dim_channel (channel_key, channel_name, channel_type)
-VALUES (7, 'Corporate Sales', 'Agent');
-
-INSERT INTO dim_channel (channel_key, channel_name, channel_type)
-VALUES (8, 'Third-Party Websites', 'Online');
-
-INSERT INTO dim_channel (channel_key, channel_name, channel_type)
-VALUES (9, 'Kiosks', 'Offline');
-
-INSERT INTO dim_channel (channel_key, channel_name, channel_type)
-VALUES (10, 'In-Flight', 'Offline');
