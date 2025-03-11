2fd166d50862bf8299d339ac7169a0579a4cd56f
Mohamed Ashraf <93071951+TmohamedashrafT@users.noreply.github.com>
2025-03-12 00:11:31 +0200

    Update create_dim_interaction.sql

--- physical schema/dim_interaction/create_dim_interaction.sql
+++ physical schema/dim_interaction/create_dim_interaction.sql
@@ -2,6 +2,5 @@ CREATE TABLE dim_interaction (
     interaction_key INT,
     interaction_type VARCHAR2(255),
     severity VARCHAR2(255),
-    engine_type VARCHAR2(255),
-    status VARCHAR2(255)
+    interaction_channel VARCHAR2(255)
 );
