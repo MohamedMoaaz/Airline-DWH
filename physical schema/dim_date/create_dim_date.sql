c84a76200e88abb6a2f0c39be11d0b4f535b16ec
Mahmuuod <78619567+Mahmuuod@users.noreply.github.com>
2025-03-12 17:07:20 +0200

    Update create_dim_date.sql

--- physical schema/dim_date/create_dim_date.sql
+++ physical schema/dim_date/create_dim_date.sql
@@ -1,10 +1,10 @@
 CREATE TABLE DWH_Project.dim_date (
-    DateKey         INT64,       -- Surrogate Key in YYYYMMDD format
-    Full_date       DATE,        -- Actual Date
-    DayNumber       INT64,       -- Day of the month (1-31)
-    DayName         STRING,      -- Name of the day (e.g., 'Monday')
-    monthName       STRING,      -- Name of the month (e.g., 'March')
-    yearNo          INT64,       -- Year (e.g., 2025)
-    season          STRING,      -- Season (e.g., 'Winter', 'Spring')
-    quarter         INT64        -- Quarter (1-4)
+    DateKey         INT64,    
+    Full_date       DATE,      
+    DayNumber       INT64,      
+    DayName         STRING,     
+    monthName       STRING,     
+    yearNo          INT64,      
+    season          STRING,      
+    quarter         INT64        
 );
