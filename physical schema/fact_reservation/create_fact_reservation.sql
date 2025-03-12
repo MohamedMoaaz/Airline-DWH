f1023adb9e95155fbba314ca72dc55485ed3c4b8
Mahmuuod <78619567+Mahmuuod@users.noreply.github.com>
2025-03-12 17:11:27 +0200

    Update create_fact_reservation.sql

--- physical schema/fact_reservation/create_fact_reservation.sql
+++ physical schema/fact_reservation/create_fact_reservation.sql
@@ -1,27 +1,27 @@
-CREATE TABLE fact_reservation (
-    Reservation_Key         NUMBER(10),
-    ticket_id               NUMBER(10),
-    channel_key             NUMBER(10),
-    promotion_key           NUMBER(10),
-    passenger_key           NUMBER(10),
-    fare_basis_key          NUMBER(10),
-    aircraft_key            NUMBER(10),
-    source_airport          NUMBER(10),
-    destination_airport     NUMBER(10),
-    
-    reservation_date_key    NUMBER(8),  
-    departure_date_key      NUMBER(8),   
+CREATE TABLE DWH_Project.fact_reservation (
+    Reservation_Key         INT64,
+    ticket_id               INT64,
+    channel_key             INT64,
+    promotion_key           INT64,
+    passenger_key           INT64,
+    fare_basis_key          INT64,
+    aircraft_key            INT64,
+    source_airport          INT64,
+    destination_airport     INT64,
+
+    reservation_date_key    INT64,  
+    departure_date_key      INT64,   
     departure_time          TIMESTAMP,    
     Reservation_timestamp   TIMESTAMP,    
-    
-    payment_method          VARCHAR2(50),
-    seat_no                 VARCHAR2(10),
-    
-    Promotion_Amount        NUMBER(10,2),
-    tax_amount              NUMBER(10,2) ,
-    Operational_Fees        NUMBER(10,2) ,
-    Cancelation_Fees        NUMBER(10,2) ,
-    Fare_Price              NUMBER(10,2),
-    Final_Price             NUMBER(10,2),  
-    Is_Cancelled            NUMBER(1)    
+
+    payment_method          STRING,
+    seat_no                 STRING,
+
+    Promotion_Amount        NUMERIC(10,2),
+    tax_amount              NUMERIC(10,2),
+    Operational_Fees        NUMERIC(10,2),
+    Cancelation_Fees        NUMERIC(10,2),
+    Fare_Price              NUMERIC(10,2),
+    Final_Price             NUMERIC(10,2),  
+    Is_Cancelled            NUMMERIC(1)
 );
