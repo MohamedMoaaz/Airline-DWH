951393d14805473936d0c5c8ebde4dd7939c265b
Mahmuuod <78619567+Mahmuuod@users.noreply.github.com>
2025-03-12 17:10:26 +0200

    Update create_dim_promotions.sql

--- physical schema/dim_promotion/create_dim_promotions.sql
+++ physical schema/dim_promotion/create_dim_promotions.sql
@@ -1,11 +1,11 @@
-CREATE TABLE dim_promotions (
-    promotion_id INT,
-    promotion_key INT,
-    promotion_type VARCHAR2(255),
-    promotion_target_segment VARCHAR2(255),
-    promotion_channel VARCHAR2(255),
+CREATE TABLE DWH_Project.dim_promotions (
+    promotion_id INT64,
+    promotion_key INT64,
+    promotion_type STRING,
+    promotion_target_segment STRING,
+    promotion_channel STRING,
     promotion_start_date DATE,
     promotion_end_date DATE,
-     is_current CHAR(1),
-    discount NUMBER(10, 2)
+    is_current STRING(1),
+    discount NUMERIC(10,2)
 );
