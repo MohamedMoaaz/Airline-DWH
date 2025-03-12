84868950da2476e3bb527b830c44809826b5002d
Mohamed Ashraf <93071951+TmohamedashrafT@users.noreply.github.com>
2025-03-12 18:00:25 +0200

    Update create_dim_employee.sql to run on bigquery

--- physical schema/dim_employee/create_dim_employee.sql
+++ physical schema/dim_employee/create_dim_employee.sql
@@ -1,11 +1,11 @@
 CREATE TABLE dim_employee (
-    employee_key INT,
-    employee_id INT,
-    employee_first_name VARCHAR2(255),
-    employee_last_name VARCHAR2(255),
-    employee_email VARCHAR2(255),
-    employee_phone_number VARCHAR2(20),
-    employee_job_title VARCHAR2(255),
-    employee_salary NUMBER(10,2),
+    employee_key INT64,
+    employee_id INT64,
+    employee_first_name STRING,
+    employee_last_name STRING,
+    employee_email STRING,
+    employee_phone_number STRING,
+    employee_job_title STRING,
+    employee_salary NUMERIC(10, 2),
     employee_hiring_date DATE
 );
