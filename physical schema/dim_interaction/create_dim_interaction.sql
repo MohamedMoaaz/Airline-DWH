ef95df9496e98f414a41e6c0773f17784401bfa3
Mohamed Ashraf <93071951+TmohamedashrafT@users.noreply.github.com>
2025-03-12 18:03:41 +0200

    Update create_dim_interaction.sql to run on bigquery

--- physical schema/dim_interaction/create_dim_interaction.sql
+++ physical schema/dim_interaction/create_dim_interaction.sql
@@ -1,6 +1,6 @@
-CREATE TABLE dim_interaction (
-    interaction_key INT,
-    interaction_type VARCHAR2(255),
-    severity VARCHAR2(255),
-    interaction_channel VARCHAR2(255)
+CREATE TABLE DWH_Project.dim_interaction (
+    interaction_key INT64,
+    interaction_type STRING,
+    severity STRING,
+    interaction_channel STRING
 );
