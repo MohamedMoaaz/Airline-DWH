b8553b62ee5a0294dff39be33e595de72522a59c
Mennatullahatta <mennaatta94@gmail.com>
2025-03-12 17:17:55 +0200

    Update create_dim_services.sql

--- physical schema/dim_service/create_dim_services.sql
+++ physical schema/dim_service/create_dim_services.sql
@@ -1,10 +1,10 @@
-CREATE TABLE dim_services (
-    service_key INT,
-    service_id INT,
-    service_type VARCHAR2(255),
-    service_category VARCHAR2(255),
-    service_cost NUMBER(10, 2),
-    service_location VARCHAR2(255),
+CREATE TABLE DWH_Project.dim_services (
+    service_key INT64,
+    service_id INT64,
+    service_type STRING(255),
+    service_category STRING(255),
+    service_cost NUMERIC(10, 2),
+    service_location STRING(255),
     start_date DATE,
     end_date DATE
 );
