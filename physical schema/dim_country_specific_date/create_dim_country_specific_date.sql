d76c9f9d4dcbb059ccedb86f9be2a2d131fb086d
Mohamed Ashraf <93071951+TmohamedashrafT@users.noreply.github.com>
2025-03-13 01:09:00 +0200

    Update create_dim_country_specific_date.sql to run on bigquery

--- physical schema/dim_country_specific_date/create_dim_country_specific_date.sql
+++ physical schema/dim_country_specific_date/create_dim_country_specific_date.sql
@@ -1,12 +1,12 @@
-CREATE TABLE dim_country_specific_date (
-    date_key NUMBER,
-    country_key NUMBER,
-    country_name VARCHAR2(100),
-    civil_name VARCHAR2(100),
-    civil_holiday_flag CHAR(1),
-    civil_holiday_name VARCHAR2(100),
-    religious_holiday_flag CHAR(1),
-    religious_holiday_name VARCHAR2(100),
-    weekday_indicator VARCHAR2(50),
-    season_name VARCHAR2(50)
+CREATE TABLE DWH_Project.dim_country_specific_date (
+    date_key INT64,
+    country_key INT64,
+    country_name STRING,
+    civil_name STRING,
+    civil_holiday_flag STRING,
+    civil_holiday_name STRING,
+    religious_holiday_flag STRING,
+    religious_holiday_name STRING,
+    weekday_indicator STRING,
+    season_name STRING
 );
