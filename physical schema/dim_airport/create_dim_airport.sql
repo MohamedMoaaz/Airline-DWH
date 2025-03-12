f30edaa795d274bbedcce84f85a060fe1b75e77d
Mahmuuod <78619567+Mahmuuod@users.noreply.github.com>
2025-03-12 17:05:39 +0200

    Update create_dim_airport.sql

--- physical schema/dim_airport/create_dim_airport.sql
+++ physical schema/dim_airport/create_dim_airport.sql
@@ -1,15 +1,15 @@
-CREATE TABLE dim_airport (
-    airport_key NUMBER,
-    airport_id NUMBER,
-    airport_name VARCHAR2(100),
-    airport_code VARCHAR2(10),
-    airport_city VARCHAR2(100),
-    airport_country VARCHAR2(100),
-    airport_region VARCHAR2(100),
-    airport_type VARCHAR2(50),
-    airport_latitude NUMBER,
-    airport_longitude NUMBER,
-    airport_hub_status VARCHAR2(20),
-    airport_no_of_runways NUMBER,
-    airport_no_of_terminals NUMBER
+CREATE TABLE DWH_Project.dim_airport (
+    airport_key INT64,
+    airport_id INT64,
+    airport_name STRING,
+    airport_code STRING,
+    airport_city STRING,
+    airport_country STRING,
+    airport_region STRING,
+    airport_type STRING,
+    airport_latitude FLOAT64,
+    airport_longitude FLOAT64,
+    airport_hub_status STRING,
+    airport_no_of_runways INT64,
+    airport_no_of_terminals INT64
 );
