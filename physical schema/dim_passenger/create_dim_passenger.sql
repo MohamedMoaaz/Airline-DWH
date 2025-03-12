0efd44b1155ac4b7a40210b2bfb4a7714a187c1f
Mahmuuod <78619567+Mahmuuod@users.noreply.github.com>
2025-03-12 17:08:47 +0200

    Update create_dim_passenger.sql

--- physical schema/dim_passenger/create_dim_passenger.sql
+++ physical schema/dim_passenger/create_dim_passenger.sql
@@ -1,17 +1,17 @@
-CREATE TABLE dim_passenger (
-    passenger_key NUMBER,
-    passenger_id NUMBER,
-    passenger_national_id VARCHAR2(50),
-    passenger_passport_id VARCHAR2(50),
-    passenger_firstname VARCHAR2(100),
-    passenger_lastname VARCHAR2(100),
+CREATE TABLE DWH_Project.dim_passenger (
+    passenger_key INT64,
+    passenger_id INT64,
+    passenger_national_id STRING,
+    passenger_passport_id STRING,
+    passenger_firstname STRING,
+    passenger_lastname STRING,
     passenger_dob DATE,
-    passenger_city VARCHAR2(100),
-    passenger_nationality VARCHAR2(100),
-    passenger_country VARCHAR2(100),
-    passenger_email VARCHAR2(100),
-    passenger_phoneno VARCHAR2(20),
-    passenger_gender VARCHAR2(10),
-    passenger_language VARCHAR2(50),
-    passenger_marital_status VARCHAR2(20)
+    passenger_city STRING,
+    passenger_nationality STRING,
+    passenger_country STRING,
+    passenger_email STRING,
+    passenger_phoneno STRING,
+    passenger_gender STRING,
+    passenger_language STRING,
+    passenger_marital_status STRING
 );
