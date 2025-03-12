3d633167d0c28bd51eaef899ca87be9e6c35833f
Mahmuuod <78619567+Mahmuuod@users.noreply.github.com>
2025-03-12 17:09:15 +0200

    Update create_dim_passenger_profile_current.sql

--- physical schema/dim_passenger_profile/create_dim_passenger_profile_current.sql
+++ physical schema/dim_passenger_profile/create_dim_passenger_profile_current.sql
@@ -1,7 +1,7 @@
-CREATE TABLE dim_passenger_profile (
-    profile_key NUMBER,
-    frequent_flyer_tier VARCHAR2(50),
-    home_airport VARCHAR2(100),
-    lifetime_mileage_tier VARCHAR2(50),
+CREATE TABLE DWH_Project.dim_passenger_profile (
+    profile_key INT64,
+    frequent_flyer_tier STRING,
+    home_airport STRING,
+    lifetime_mileage_tier STRING,
     updated_date DATE
 );
