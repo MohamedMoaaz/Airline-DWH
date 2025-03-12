0d948c18953c5b33896a3549d2de84ff352fd404
Mahmuuod <78619567+Mahmuuod@users.noreply.github.com>
2025-03-12 17:09:42 +0200

    Update create_dim_passenger_profile_history.sql

--- physical schema/dim_passenger_profile/create_dim_passenger_profile_history.sql
+++ physical schema/dim_passenger_profile/create_dim_passenger_profile_history.sql
@@ -1,9 +1,9 @@
-CREATE TABLE dim_passenger_profile_history (
-    profile_history_key NUMBER,
-    profile_key NUMBER,
-    frequent_flyer_tier VARCHAR2(50),
-    home_airport VARCHAR2(100),
-    lifetime_mileage_tier VARCHAR2(50),
-    start_date Date,
-    end_date Date
+CREATE TABLE DWH_Project.dim_passenger_profile_history (
+    profile_history_key INT64,
+    profile_key INT64,
+    frequent_flyer_tier STRING,
+    home_airport STRING,
+    lifetime_mileage_tier STRING,
+    start_date DATE,
+    end_date DATE
 );
