4ef19984ed7f8ee15f3be2d73999fce2e880698c
Mohamed Ashraf <93071951+TmohamedashrafT@users.noreply.github.com>
2025-03-12 18:02:00 +0200

    Update create_crew_employee_bridge.sql to run on bigquery

--- physical schema/crew_employee_bridge/create_crew_employee_bridge.sql
+++ physical schema/crew_employee_bridge/create_crew_employee_bridge.sql
@@ -1,5 +1,5 @@
-CREATE TABLE crew_employee_bridge (
-    crew_employee_bridge_key INT,
-    crew_key INT,
-    employee_key INT
+CREATE TABLE DWH_Project.crew_employee_bridge (
+    crew_employee_bridge_key INT64,
+    crew_key INT64,
+    employee_key INT64
 );
