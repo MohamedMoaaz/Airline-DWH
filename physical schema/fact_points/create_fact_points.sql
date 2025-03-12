97b359468027dfdd58730e97ecdd7b53c8f65013
Mennatullahatta <mennaatta94@gmail.com>
2025-03-12 17:11:25 +0200

    Update create_fact_points.sql

--- physical schema/fact_points/create_fact_points.sql
+++ physical schema/fact_points/create_fact_points.sql
@@ -1,15 +1,15 @@
- CREATE TABLE fact_points (
-    points_key NUMBER,
-    passenger_key NUMBER,
-    passenger_profile_key NUMBER,
-    transaction_date_key NUMBER,
-    expiration_date_key NUMBER,
-    aircraft_key NUMBER,
-    crew_employee_bridge_key NUMBER,
-    service_key NUMBER,
-    promotion_key NUMBER,
-    points_earned NUMBER,
-    points_redeemed NUMBER,
-    points_expired NUMBER
+ CREATE TABLE DWH_Project.fact_points (
+    points_key int64,
+    passenger_key int64,
+    passenger_profile_key int64,
+    transaction_date_key int64,
+    expiration_date_key int64,
+    aircraft_key int64,
+    crew_employee_bridge_key int64,
+    service_key int64,
+    promotion_key int64,
+    points_earned int64,
+    points_redeemed int64,
+    points_expired int64
     
 );
