0247fa1cd9697fa367dad24c14c9836bd792d20a
Mahmuuod <78619567+Mahmuuod@users.noreply.github.com>
2025-03-12 17:03:40 +0200

    Update create_dim_aircraft.sql

--- physical schema/dim_aircraft/create_dim_aircraft.sql
+++ physical schema/dim_aircraft/create_dim_aircraft.sql
@@ -1,14 +1,13 @@
-CREATE TABLE dim_aircraft (
-    aircraft_key INT,
-    aircraft_model VARCHAR(50),
-    aircraft_manufacturer VARCHAR(50),
-    aircraft_capacity INT,
-    aircraft_enginetype VARCHAR(50),
-    aircraft_status VARCHAR(20),
-    economy_seats_range VARCHAR(20),
-    business_seats_range VARCHAR(20),
-    firstclass_seats_range VARCHAR(20),
-    max_miles INT,
-    max_speed INT
+CREATE TABLE DWH_Project.dim_aircraft (
+    aircraft_key            INT64,       -- Surrogate Key
+    aircraft_model          STRING,      -- Aircraft model name
+    aircraft_manufacturer   STRING,      -- Manufacturer name
+    aircraft_capacity       INT64,       -- Total seating capacity
+    aircraft_enginetype     STRING,      -- Engine type
+    aircraft_status         STRING,      -- Active, Retired, etc.
+    economy_seats_range     STRING,      -- Example: "150-200"
+    business_seats_range    STRING,      -- Example: "30-50"
+    firstclass_seats_range  STRING,      -- Example: "10-20"
+    max_miles               INT64,       -- Maximum range in miles
+    max_speed               INT64        -- Maximum speed in mph/kmph
 );
-
