1f54089e92cf0ec756caa49b0d8879e382412e51
Mahmuuod <78619567+Mahmuuod@users.noreply.github.com>
2025-03-12 17:08:15 +0200

    Update create_dim_farebasis.sql

--- physical schema/dim_farebasis/create_dim_farebasis.sql
+++ physical schema/dim_farebasis/create_dim_farebasis.sql
@@ -1,14 +1,12 @@
-CREATE TABLE dim_farebasis (
-    farebasis_key NUMBER, 
-    farebasis_code VARCHAR2(50) ,
-    farebasis_class VARCHAR2(50),
-    farebasis_type VARCHAR2(50),
-    advance_purchase_required CHAR(1),
-    advance_purchase_days NUMBER,
-    farebasis_is_restricted CHAR(1),
-    farebasis_restrictions VARCHAR2(500),
-    farebasis_change_penalty NUMBER(10, 2),
-    farebasis_cancellation_penalty NUMBER(10, 2)
+CREATE TABLE DWH_Project.dim_farebasis (
+    farebasis_key INT64, 
+    farebasis_code STRING,
+    farebasis_class STRING,
+    farebasis_type STRING,
+    advance_purchase_required STRING(1),
+    advance_purchase_days INT64,
+    farebasis_is_restricted STRING(1),
+    farebasis_restrictions STRING,
+    farebasis_change_penalty NUMERIC(10,2),
+    farebasis_cancellation_penalty NUMERIC(10,2)
 );
-
-
